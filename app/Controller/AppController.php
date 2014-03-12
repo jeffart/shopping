46ee07d05b9e3573f92a72954bbe07e426e23e3c
jeffart <dekogha@gmail.com>
2014-03-12 09:51:53 +0100

    5- Activation du layout admin pour les vues admin

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,4 +32,10 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    public function beforeFilter() {
+       
+        if (isset($this->request->params['prefix']) && $this->request->params['prefix'] == 'admin')
+            $this->layout = 'admin';
+    }
 }
